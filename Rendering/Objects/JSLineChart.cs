144cddd21a4efb6ad5aed46166c6e345331fb26f
Naxesss <30292137+Naxesss@users.noreply.github.com>
2021-09-21 22:30:18 +0200

    Add chart objects for serialization

---
+++ Rendering/Objects/JSLineChart.cs
@@ -0,0 +1,51 @@
+﻿using MapsetVerifierFramework.objects.components;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Numerics;
+using System.Reflection.Emit;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace MapsetVerifierBackend.Rendering.Objects
+{
+    public class JSLineChart : JSChart<Point, Series>
+    {
+        // https://www.chartjs.org/docs/latest/getting-started/usage.html
+
+        /*
+         *  Json Seralization Mapping
+         *  Note: Capitalization of variables matter; they must be lowercased.
+        */
+
+        protected struct DataSet : IDataSet
+        {
+            public string      label           { get; set; }
+            public List<Point> data            { get; set; }
+            public string      backgroundColor { get; set; }
+            public string      borderColor     { get; set; }
+            public bool        fill            { get; set; }
+        }
+
+        /*
+         *  Attributes & Constructors
+        */
+
+        public JSLineChart(LineChart lineChart)
+            : base(lineChart)
+        { }
+
+        protected override IDataSet ToDataSet(Series series)
+        {
+            return new DataSet()
+            {
+                label           = series.Label,
+                data            = series.Points.Select(vec => new Point(vec)).ToList(),
+                backgroundColor = $"rgba({series.Color.R}, {series.Color.G}, {series.Color.B}, {series.Color.A / 255f})",
+                borderColor     = $"rgba({series.Color.R}, {series.Color.G}, {series.Color.B}, {series.Color.A / 255f})",
+                fill            = false
+            };
+        }
+    }
+}
---
+++ Rendering/Objects/Point.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace MapsetVerifierBackend.Rendering.Objects
+{
+    public class Point
+    {
+        /*
+         *  Note: Capitalization of variables matter for serialization; they must be lowercased.
+        */
+
+        public double x { get; set; }
+        public double y { get; set; }
+
+        public Point(Vector2 vec)
+        {
+            x = vec.X;
+            y = vec.Y;
+        }
+    }
+}
